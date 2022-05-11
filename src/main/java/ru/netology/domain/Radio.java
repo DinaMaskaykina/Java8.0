cdd559ecf1526bfd81e6fc344fadf46f65e6e429
Dina <maskaykinadina@gmail.com>
2022-05-11 15:55:56 +0300

    добавила тесты

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,4 +1,59 @@
 package ru.netology.domain;
 
 public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        this.currentStation = currentStation;
+    }
+
+    public void next() {
+        this.currentStation = currentStation + 1;
+        if (currentStation == 10) {
+            this.currentStation = 0;
+        }
+    }
+
+    public void prev() {
+        this.currentStation = currentStation - 1;
+        if (currentStation == -1) {
+            this.currentStation = 9;
+        }
+    }
+
+    public void stationInput(int stationInput) {
+        if (stationInput < 0) {
+            return;
+        }
+        if (stationInput > 9) {
+            return;
+        }
+        this.currentStation = stationInput;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+    public void downVolume() {
+        this.currentVolume = currentVolume - 1;
+        if (currentVolume == -1) {
+            this.currentVolume = 0;
+        }
+    }
 }
+
